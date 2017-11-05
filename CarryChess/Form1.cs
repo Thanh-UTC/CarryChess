6b010e99cf9c2f6aaa0c7a5b9cd4e3197d20d459
Thanh-UTC <daokythanh.343@gmail.com>
2017-11-05 21:47:58 +0700

    - Vẽ bàn cờ - Vẽ quân cờ lên bàn cờ - Viết hàm xóa quân cờ - Tạo sự kiện click cho quân cờ

--- CarryChess/Form1.cs
+++ CarryChess/Form1.cs
@@ -12,9 +12,140 @@ namespace CarryChess
 {
     public partial class Form1 : Form
     {
+        //
+        // Attributes
+        //
+        private int[,] tempmap = CONS.MAP;
+        private PictureBox[,] listChess = new PictureBox[5, 5];
+        private Point destinationPoint;
+        private Point startPoint;
+        private PictureBox tempPicBox;
+        //
+        //
+        //
         public Form1()
         {
             InitializeComponent();
+            DrawChessMan(tempmap);
         }
+        //
+        //Draw the chessboard
+        //
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            //
+            // Create pen.
+            //
+            Pen blackPen = new Pen(Color.Black, 3);
+            //
+            //create point
+            //
+            Point[] point = new Point[2];
+            //
+            // vertical lines
+            //
+            for (int i = CONS.FIRST_COORD; i <= CONS.CHESS_SIZE; i += CONS.SQUARE_SIZE)
+            {
+                point[0] = new Point(i, 25);
+                point[1] = new Point(i, 525);
+                e.Graphics.DrawLines(blackPen, point);
+            }
+            //
+            //horizontal lines
+            //
+            for (int i = 25; i <= 525; i += 125)
+            {
+                point[0] = new Point(25, i);
+                point[1] = new Point(525, i);
+                e.Graphics.DrawLines(blackPen, point);
+            }
+            //
+            //italic lines
+            //
+            e.Graphics.DrawLine(blackPen, 275, 25, 25, 275);
+            e.Graphics.DrawLine(blackPen, 525, 25, 25, 525);
+            e.Graphics.DrawLine(blackPen, 525, 275, 275, 525);
+            e.Graphics.DrawLine(blackPen, 25, 25, 525, 525);
+            e.Graphics.DrawLine(blackPen, 275, 25, 525, 275);
+            e.Graphics.DrawLine(blackPen, 25, 275, 275, 525);
+        }
+        //
+        //Draw chessmen
+        //
+        public void DrawChessMan(int[,] _tempmap)
+        {
+            Point location;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    location = new Point(j * CONS.SQUARE_SIZE, i * CONS.SQUARE_SIZE);
+                    if (_tempmap[i+CONS.STD_DEVI, j+CONS.STD_DEVI] == 1)
+                    {
+                        listChess[i, j] = new PictureBox
+                        {
+                            Location = location,
+                            Image = CarryChess.Properties.Resources.QuanCoDen,
+                            BackColor = Color.Transparent
+                        };
+                        panel1.Controls.Add(listChess[i, j]);
+                        listChess[i, j].Click += ChessClick;
+                    }
+                    else if (_tempmap[i+CONS.STD_DEVI, j+CONS.STD_DEVI] == 2)
+                    {
+                        listChess[i, j] = new PictureBox
+                        {
+                            Location = location,
+                            BackColor = Color.Transparent,
+                            Image = CarryChess.Properties.Resources.QuanCoTrang
+                        };
+                        panel1.Controls.Add(listChess[i, j]);
+                        listChess[i, j].Click += ChessClick;
+                    }
+                    else
+                    {
+                        listChess[i, j] = null;
+                    }
+                }
+            }
+        }
+        //
+        //Delete all chessmen picture on the chessboard
+        //
+        public void WipeOutChessmen()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    panel1.Controls.Remove(listChess[j, i]);
+                }
+            }
+        }
+        //
+        // Picturebox click event
+        //
+        private void ChessClick(object sender, EventArgs e)
+        {
+            PictureBox chessPiece = sender as PictureBox;
+            Console.WriteLine(chessPiece.Location.X + ";" + chessPiece.Location.Y);
+            //
+            // find the coord in map- set coord to startPoint
+            //
+            startPoint = new Point(chessPiece.Location.X, chessPiece.Location.Y);
+            int i = (chessPiece.Location.X) / CONS.SQUARE_SIZE;
+            int j = (chessPiece.Location.Y) / CONS.SQUARE_SIZE;
+            Console.WriteLine(startPoint.X + ";" + startPoint.Y);
+            tempPicBox = listChess[j, i];
+            Console.WriteLine("current coord: " + tempPicBox.Location.X + ";" + tempPicBox.Location.Y);
+            Console.WriteLine("start point coord: " + j + ";" + i);
+            //
+            //preset
+            //
+            chessPiece.BackColor = Color.PeachPuff;
+            chessPiece.Size = new Size(50, 50);
+        }
+
+
     }
 }
--- CarryChess/Properties/Resources.Designer.cs
+++ CarryChess/Properties/Resources.Designer.cs
@@ -8,10 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace CarryChess.Properties
-{
-
-
+namespace CarryChess.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -22,49 +22,141 @@ namespace CarryChess.Properties
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources
-    {
-
+    internal class Resources {
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources()
-        {
+        internal Resources() {
         }
-
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager
-        {
-            get
-            {
-                if ((resourceMan == null))
-                {
+        internal static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CarryChess.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture
-        {
-            get
-            {
+        internal static global::System.Globalization.CultureInfo Culture {
+            get {
                 return resourceCulture;
             }
-            set
-            {
+            set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap AnhBanCo {
+            get {
+                object obj = ResourceManager.GetObject("AnhBanCo", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap BanCoFix {
+            get {
+                object obj = ResourceManager.GetObject("BanCoFix", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap CanMove {
+            get {
+                object obj = ResourceManager.GetObject("CanMove", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap cjj1342493794 {
+            get {
+                object obj = ResourceManager.GetObject("cjj1342493794", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap duonggachbanco {
+            get {
+                object obj = ResourceManager.GetObject("duonggachbanco", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Empty {
+            get {
+                object obj = ResourceManager.GetObject("Empty", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap KhungBanCoFix {
+            get {
+                object obj = ResourceManager.GetObject("KhungBanCoFix", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap QuanCoDen {
+            get {
+                object obj = ResourceManager.GetObject("QuanCoDen", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap QuanCoDo {
+            get {
+                object obj = ResourceManager.GetObject("QuanCoDo", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap QuanCoTrang {
+            get {
+                object obj = ResourceManager.GetObject("QuanCoTrang", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
     }
